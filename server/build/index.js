a3fb0a5ecec04d3f5d591045bf28160c0391a5d5
Paul <p@x128.ru>
2023-04-09 15:23:05 +0300

    basic ts watch config

--- server/build/index.js
+++ server/build/index.js
@@ -1,14 +1,10 @@
-'use strict'
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000
-
-app.get('/', (req, res) => {
-    res.status(200).send('<h1>OK</h1>')
-})
-
+"use strict";
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 try {
-    app.listen(PORT, () => console.log('Server listening..'))
-} catch (err) {
-    console.error(err)
+    app.listen(PORT, () => console.log('Server listening..'));
+}
+catch (err) {
+    console.error(err);
 }
