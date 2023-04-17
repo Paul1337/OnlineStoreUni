d13c88f89c02e3fec02cd485e6c5dea0203767b5
Paul <p@x128.ru>
2023-04-17 20:55:34 +0300

    very basic shop page

---
+++ client/src/reducers/store/model.ts
@@ -0,0 +1,7 @@
+interface IStoreItem {
+    id: number;
+    title: string;
+    description: string;
+    img: string;
+    price: number;
+}
--- client/src/views/pages/ShopPage/ShopPage.tsx
+++ client/src/views/pages/ShopPage/ShopPage.tsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import styles from './ShopPage.module.css';
+
+const products: IStoreItem[] = [
+    {
+        id: 0,
+        title: 'product-test',
+        description: 'product-description',
+        img: 'img',
+        price: 99,
+    },
+    {
+        id: 1,
+        title: 'product-test 2',
+        description: 'some descr',
+        img: 'img',
+        price: 99,
+    },
+    {
+        id: 2,
+        title: 'product-test 3',
+        description: 'description again',
+        img: 'img',
+        price: 299,
+    },
+    {
+        id: 3,
+        title: 'product-test 4',
+        description: 'description',
+        img: 'img',
+        price: 399,
+    },
+];
 
 const ShopPage = () => {
-    return <div>ShopPage</div>;
+    return (
+        <div className={styles.productCont}>
+            {products.map((product) => (
+                <div className={styles.product + ' bg-gray-200'} key={product.id}>
+                    <div className={styles.product__title}>{product.title}</div>
+                    <img src={product.img} alt='image not found' className={styles.product__img} />
+                    <div className={styles.product__descr}>{product.description}</div>
+                    <div className={styles.product__price}>{product.price} р.</div>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default ShopPage;
