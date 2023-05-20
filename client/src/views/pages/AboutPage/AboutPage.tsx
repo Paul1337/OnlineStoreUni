c207cba38c21bffacd56ac2269d2a396a399b8d0
Paul <p@x128.ru>
2023-05-20 17:39:42 +0300

    about page

--- client/src/views/pages/AboutPage/AboutPage.tsx
+++ client/src/views/pages/AboutPage/AboutPage.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { FunctionComponent, PropsWithChildren } from 'react';
+import styles from './AboutPage.module.css';
+
+const AboutItem: FunctionComponent<PropsWithChildren> = ({ children }) => (
+    <li className='shadow-sm bg-slate-300 p-2 m-2 font-bold'>{children}</li>
+);
 
 const AboutPage = () => {
-    return <div>AboutPage</div>;
+    return (
+        <div className={styles.main}>
+            <div className={styles.cont}>
+                <div>
+                    <div className='text-center text-2xl my-2'>Frontend</div>
+                    <ul>
+                        <AboutItem>ReactJS application</AboutItem>
+                        <AboutItem>Reduxjs toolkit state management</AboutItem>
+                        <AboutItem>Typescript</AboutItem>
+                        <AboutItem>SPA routing</AboutItem>
+                        <AboutItem>Async thunk middleware</AboutItem>
+                        <AboutItem>Tailwind styling</AboutItem>
+                    </ul>
+                </div>
+
+                <div>
+                    <div className='text-center text-2xl my-2'>Backend</div>
+                    <ul>
+                        <AboutItem>Node.js using typescript</AboutItem>
+                        <AboutItem>Express server</AboutItem>
+                        <AboutItem>Mysql database</AboutItem>
+                        <AboutItem>JWT token authorization</AboutItem>
+                        <AboutItem>Server-side email & password validation</AboutItem>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default AboutPage;
